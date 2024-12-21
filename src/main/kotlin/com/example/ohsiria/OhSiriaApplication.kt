c11edbfd87ddffe683716495cea2e0b328a63f65
4mjeo <4mj.eo@dsm.hs.kr>
2024-12-21 16:19:35 +0900

    initial commit

---
+++ src/main/kotlin/com/example/ohsiria/OhSiriaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ohsiria
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OhSiriaApplication
+
+fun main(args: Array<String>) {
+    runApplication<OhSiriaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ohsiria/OhSiriaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ohsiria
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OhSiriaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
