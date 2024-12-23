dcd36f729ebd8fe20cd14e01ae653216240e91fc
4mjeo <4mj.eo@dsm.hs.kr>
2024-12-23 12:30:19 +0900

    refactor: modify code style & add dependencies

---
+++ src/main/kotlin/com/example/ohsiria/global/config/security/SecurityConfig.kt
@@ -0,0 +1,57 @@
+package com.example.ohsiria.global.config.security
+
+import com.example.ohsiria.global.config.error.handler.ExceptionHandlerFilter
+import com.example.ohsiria.global.config.filter.TokenFilter
+import com.example.ohsiria.global.config.jwt.JwtTokenResolver
+import com.example.ohsiria.global.config.jwt.TokenProvider
+import mu.KLogger
+import mu.KotlinLogging
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import java.util.*
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val tokenProvider: TokenProvider,
+    private val exceptionHandlerFilter: ExceptionHandlerFilter,
+    private val tokenResolver: JwtTokenResolver,
+) {
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .formLogin { it.disable() }
+            .cors { it.configurationSource { CorsConfiguration().applyPermitDefaultValues() } }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+
+        http
+            .authorizeHttpRequests { authorize ->
+                authorize.requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                authorize.anyRequest().authenticated()
+            }
+
+        http.addFilterBefore(exceptionHandlerFilter, UsernamePasswordAuthenticationFilter::class.java)
+        http.addFilterAfter(TokenFilter(tokenResolver, tokenProvider), ExceptionHandlerFilter::class.java)
+
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+
+    @Bean
+    fun bankEncoder(): Base64.Encoder = Base64.getEncoder()
+
+    @Bean
+    fun logger(): KLogger = KotlinLogging.logger {}
+}
