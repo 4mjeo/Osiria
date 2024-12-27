4f632058fb7d1e9ba55a36124481839c7710e816
4mjeo <4mj.eo@dsm.hs.kr>
2024-12-27 23:59:17 +0900

    feat: add dependecny & controller

---
+++ src/main/kotlin/com/example/ohsiria/domain/reservation/presentation/ReservationController.kt
@@ -0,0 +1,22 @@
+package com.example.ohsiria.domain.reservation.presentation
+
+import com.example.ohsiria.domain.reservation.presentation.dto.ReserveRequest
+import com.example.ohsiria.domain.reservation.service.ReserveService
+import jakarta.validation.Valid
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/reservation")
+@Validated
+class ReservationController(
+    private val reserveService: ReserveService
+) {
+    @PostMapping
+    fun reserve(@RequestBody @Valid request: ReserveRequest) {
+        reserveService.execute(request)
+    }
+}
