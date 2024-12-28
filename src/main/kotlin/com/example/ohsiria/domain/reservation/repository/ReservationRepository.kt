7cf9ea0f776c7a420e232678722521a1c3335cc3
4mjeo <4mj.eo@dsm.hs.kr>
2024-12-28 13:06:16 +0900

    chore: delete unnecessary code

--- src/main/kotlin/com/example/ohsiria/domain/reservation/repository/ReservationRepository.kt
+++ src/main/kotlin/com/example/ohsiria/domain/reservation/repository/ReservationRepository.kt
@@ -3,10 +3,8 @@ package com.example.ohsiria.domain.reservation.repository
 import com.example.ohsiria.domain.reservation.entity.Reservation
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.time.LocalDate
 import java.util.*
 
 @Repository
 interface ReservationRepository: JpaRepository<Reservation, UUID?> {
-    fun existsByStartDateAndEndDate(startDate: LocalDate, endDate: LocalDate): Boolean
 }
