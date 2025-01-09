e7a45f4b92122e8c32dca59e4caba7f457ddc567
4mjeo <4mj.eo@dsm.hs.kr>
2025-01-10 00:36:01 +0900

    feat: setting about s3

---
+++ src/main/kotlin/com/example/ohsiria/global/env/s3/S3Property.kt
@@ -0,0 +1,12 @@
+package com.example.ohsiria.global.env.s3
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("cloud.aws.s3")
+data class S3Property(
+    val bucket: String,
+    val dir: String,
+    val accessKey: String,
+    val secretKey: String,
+    val region: String
+)
