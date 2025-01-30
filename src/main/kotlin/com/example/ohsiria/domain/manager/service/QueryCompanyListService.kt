8678469755c362b0c9dfd0a6c67b141251b2e74a
4mjeo <4mj.eo@dsm.hs.kr>
2025-01-30 18:25:49 +0900

    chore: apply aesExcrypt

--- src/main/kotlin/com/example/ohsiria/domain/manager/service/QueryCompanyListService.kt
+++ src/main/kotlin/com/example/ohsiria/domain/manager/service/QueryCompanyListService.kt
@@ -2,12 +2,14 @@ package com.example.ohsiria.domain.manager.service
 
 import com.example.ohsiria.domain.company.repository.CompanyRepository
 import com.example.ohsiria.domain.manager.presentation.dto.response.CompanyListResponse
+import com.example.ohsiria.global.config.security.AESEncryptionService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class QueryCompanyListService(
     private val companyRepository: CompanyRepository,
+    private val aesEncryptionService: AESEncryptionService,
 ) {
     @Transactional(readOnly = true)
     fun execute(): List<CompanyListResponse> {
@@ -15,7 +17,7 @@ class QueryCompanyListService(
             CompanyListResponse(
                 it.user.name,
                 it.user.accountId,
-                it.user.password
+                aesEncryptionService.decrypt(it.user.password)
             )
         }
     }
